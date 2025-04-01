add0ff96687be78436fa53b17f8a355b8d7d3d9f
agrwldaya <agrawaldaya336@gmail.com>
2025-04-01 14:41:02 +0530

    hero

--- Frontend/src/components/Home/Hero.jsx
+++ Frontend/src/components/Home/Hero.jsx
@@ -29,7 +29,7 @@ const Hero = () => {
         </div>
 
         {/* About Section */}
-        <div className="mt-5 flex flex-col lg:flex-row items-center gap-12 lg:gap-20">
+        <div className="mt-10 flex flex-col lg:flex-row items-center gap-12 lg:gap-20">
           {/* Profile Image */}
           <div className="md:w-1/3 lg:w-1/2 flex  ">
             <div className="relative group">
@@ -38,7 +38,7 @@ const Hero = () => {
                 <img 
                   src={img03}
                   alt="Dr. Pawan Verma" 
-                  className="w-full h-full  object-cover"
+                  className="w-full h-full mt-5  object-cover"
                 />
                 {/* Overlay gradient */}
                 <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent" />
