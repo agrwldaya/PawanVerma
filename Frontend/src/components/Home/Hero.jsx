b3e4bf453320d714d09e52da1df07e6c41affcb1
agrwldaya <agrawaldaya336@gmail.com>
2025-04-01 15:20:26 +0530

    hero

--- Frontend/src/components/Home/Hero.jsx
+++ Frontend/src/components/Home/Hero.jsx
@@ -34,11 +34,11 @@ const Hero = () => {
           <div className="md:w-1/3 lg:w-1/2 flex  ">
             <div className="relative group">
               {/* Main image container */}
-              <div className="relative  w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 rounded-full overflow-hidden border-4 border-[#d7a547] shadow-2xl transition-transform duration-500 hover:scale-105">
+              <div className="relative mt-5 mb-2  w-64 h-64 sm:w-80 sm:h-80 md:w-96 md:h-96 rounded-full overflow-hidden border-4 border-[#d7a547] shadow-2xl transition-transform duration-500 hover:scale-105">
                 <img 
                   src={img03}
                   alt="Dr. Pawan Verma" 
-                  className="w-full h-full mt-5  object-cover"
+                  className="w-full h-full mt-15 object-cover"
                 />
                 {/* Overlay gradient */}
                 <div className="absolute inset-0 bg-gradient-to-t from-black/20 to-transparent" />
